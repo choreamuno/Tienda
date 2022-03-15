27307771600a4a3d9928f4a09f3d4ac7707614c3
Alejandro Chavarria Cascante <alejandrochavarria667@gmail.com>
2022-03-14 21:23:53 -0600

    Pagina Inicial v1.0

---
+++ src/main/java/com/proyecto_Grupo1/Grupo1/Grupo1Application.java
@@ -0,0 +1,13 @@
+package com.proyecto_Grupo1.Grupo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo1Application.class, args);
+	}
+
+}
